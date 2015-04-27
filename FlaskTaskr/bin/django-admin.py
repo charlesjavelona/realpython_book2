7c0b57bc9c960bc206428f36fc84299d407b5407
charlesakio <charlesjavelona@gmail.com>
2015-04-27 18:03:16 -0400

    Added bootstrap and test units

---
+++ FlaskTaskr/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/charlesjavelona/Desktop/RealPythonAllParts-Beta-v6/Python2/FlaskTaskr/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
